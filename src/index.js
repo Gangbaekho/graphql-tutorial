0fc9b16b4b570b396665c9fa1e1a0d5fec8a612e
Gangbaekho <roorooro9933@gmail.com>
2020-09-08 15:41:10 +0900

    working with array part1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import { GraphQLServer} from 'graphql-yoga'
 // Type definitions (schema,datastructure)
 const typeDefs = `
     type Query  {
-      add(arg1:Float!,arg2:Float!):Float!
+      add(numbers:[Float!]!):Float!
+      grades:[Int!]!
     }
 
     type User {
@@ -24,9 +25,19 @@ const typeDefs = `
 const resolvers = {
     Query:{
     add(parent,args,ctx,info){
-        if(args.arg1 && args.arg2){
-            return args.arg1 + args.arg2
-            }
+        if(args.numbers.length === 0){
+            return 0
+        }
+        // [1,5,10,2]
+        // reduce에 대해서 알아야 하는건데, 첫번째는
+        // accumulator = 1, currentValue = 5가 되고 그 다음에는
+        // accumulator = 6, currentValue = 10이 된다는 것을 알면 된다.
+        return args.numbers.reduce((accumulator,currentValue)=>{
+            return accumulator + currentValue
+        })
+    },
+    grades(parent,args,ctx,info){
+        return [98,12,51]
         }
     }
 }
@@ -40,5 +51,4 @@ server.start(()=>{
     console.log('The server is up')
 })
 
-console.log('git playground branch')
-console.log('git master branch')
+
