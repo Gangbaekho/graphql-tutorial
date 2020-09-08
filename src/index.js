d7a37d0b6111fef4ebb96e006a26cb557bacb37a
Gangbaekho <roorooro9933@gmail.com>
2020-09-08 16:03:23 +0900

    working with arrays part2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import { GraphQLServer} from 'graphql-yoga'
 
+// Demo user data
+const users = [
+    {
+        id:'1',
+        name:'Andrew',
+        email:'andrew@example.com',
+        age:27
+    },
+    {
+        id:'2',
+        name:'Sarah',
+        email:'sara@example.com'
+    },
+    {
+        id:'3',
+        name:'Jinsoo',
+        email:'jinsoo@example.com',
+        age:28
+    }
+]
+
 // Type definitions (schema,datastructure)
 const typeDefs = `
     type Query  {
-      add(numbers:[Float!]!):Float!
-      grades:[Int!]!
+      users(query:String):[User!]!
+      me:User!
     }
 
     type User {
@@ -24,20 +45,22 @@ const typeDefs = `
 // Resolvers(function)
 const resolvers = {
     Query:{
-    add(parent,args,ctx,info){
-        if(args.numbers.length === 0){
-            return 0
-        }
-        // [1,5,10,2]
-        // reduce에 대해서 알아야 하는건데, 첫번째는
-        // accumulator = 1, currentValue = 5가 되고 그 다음에는
-        // accumulator = 6, currentValue = 10이 된다는 것을 알면 된다.
-        return args.numbers.reduce((accumulator,currentValue)=>{
-            return accumulator + currentValue
-        })
-    },
-    grades(parent,args,ctx,info){
-        return [98,12,51]
+        me(parent,args,ctx,info){
+            return {
+                id:'myId',
+                name:'jinsoo',
+                age:28,
+                email:'roorooro9933@gmail.com'
+            }
+        },
+        users(parent,args,ctx,info){
+            if(!args.query){
+                return users
+            }
+
+            return users.filter((user)=>{
+                return user.name.toUpperCase().includes(args.query.toUpperCase())
+            })
         }
     }
 }
