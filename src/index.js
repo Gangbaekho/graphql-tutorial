05b92ed126bd752e387f7c700b304b01b3342af6
Gangbaekho <roorooro9933@gmail.com>
2020-09-08 09:44:23 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import { GraphQLServer} from 'graphql-yoga'
+
+// Type definitions (schema,datastructure)
+const typeDefs = `
+    type Query  {
+      add(arg1:Float!,arg2:Float!):Float!
+    }
+
+    type User {
+        id: ID!
+        name:String!
+        email:String!
+        age:Int
+    }
+
+    type Post {
+        id:ID!
+        title:String!
+        body:String!
+        published:Boolean!
+    }
+`
+// Resolvers(function)
+const resolvers = {
+    Query:{
+    add(parent,args,ctx,info){
+        if(args.arg1 && args.arg2){
+            return args.arg1 + args.arg2
+            }
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(()=>{
+    console.log('The server is up')
+})
