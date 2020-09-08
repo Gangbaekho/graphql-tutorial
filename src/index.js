c4cdf8ac13ff49475abef60e835b98cb305e465a
Gangbaekho <roorooro9933@gmail.com>
2020-09-08 16:58:12 +0900

    relation with graphql

--- src/index.js
+++ src/index.js
@@ -1,31 +1,58 @@
 import { GraphQLServer} from 'graphql-yoga'
 
-// Challenge!
-
-const posts = [
+// Demo user data
+const users = [
     {
         id:'1',
-        title:'JPA Study',
-        body:'jpa was so brilliant solution to access database!',
-        published:true
+        name:'Andrew',
+        email:'andrew@example.com',
+        age:27
     },
     {
         id:'2',
-        title:'I preper mybatis than jpa',
-        body:'jpa has a 1 level caching problem, so i like mybatis althoug its pretty annoying.',
-        published:false
+        name:'Sarah',
+        email:'sara@example.com'
     },
     {
         id:'3',
-        title:'What about React?',
-        body:'react is a good front-end framework and most popular one in the world!',
-        published:true
+        name:'Jinsoo',
+        email:'jinsoo@example.com',
+        age:28
     }
 ]
+
+const posts = [
+    {
+     id:'100',
+     title:'Leaning JPA',
+     body:'Learning JPA for fun',
+     published : false,
+     author:'1'
+    },
+    {
+        id:'101',
+        title:'Leaning Hibernate',
+        body:'Learning Hibernate for fun',
+        published : false,
+        author:'1'
+    }
+    ,
+    {
+        id:'101',
+        title:'React is fun',
+        body:'Why dont you leanrn progrmming skills',
+        published : true,
+        author:'2'
+    }
+    
+]
+
 // Type definitions (schema,datastructure)
 const typeDefs = `
     type Query  {
-      post(query:String):[Post!]!
+      users(query:String):[User!]!
+      post:[Post!]!
+      me:User!
     }
 
     type User {
@@ -40,21 +67,40 @@ const typeDefs = `
         title:String!
         body:String!
         published:Boolean!
+        author:User!
     }
 `
 // Resolvers(function)
 const resolvers = {
     Query:{
-      post(parent,args,ctx,info){
-          if(!args.query){
-              return posts
-          }
+        me(parent,args,ctx,info){
+            return {
+                id:'myId',
+                name:'jinsoo',
+                age:28,
+                email:'roorooro9933@gmail.com'
+            }
+        },
+        users(parent,args,ctx,info){
+            if(!args.query){
+                return users
+            }
 
-          return posts.filter((post)=>{
-            return post.title.toLowerCase().includes(args.query.toLowerCase()) ||
-                post.body.toLowerCase().includes(args.query.toLowerCase())
-          })
-      }
+            return users.filter((user)=>{
+                return user.name.toUpperCase().includes(args.query.toUpperCase())
+            })
+        },
+        post(parent,args,ctx,info){
+            return posts
+        }
+    },
+    Post:{
+        // 모든 post들이 이 author method를 call 할 것이다.
+        author(parent,args,ctx,info){
+            return users.find((user)=>{
+                return user.id === parent.author
+            })
+        }
     }
 }
 
@@ -66,80 +112,5 @@ const server = new GraphQLServer({
 server.start(()=>{
     console.log('The server is up')
 })
-
-
-// Demo user data
-// const users = [
-//     {
-//         id:'1',
-//         name:'Andrew',
-//         email:'andrew@example.com',
-//         age:27
-//     },
-//     {
-//         id:'2',
-//         name:'Sarah',
-//         email:'sara@example.com'
-//     },
-//     {
-//         id:'3',
-//         name:'Jinsoo',
-//         email:'jinsoo@example.com',
-//         age:28
-//     }
-// ]
-
-// // Type definitions (schema,datastructure)
-// const typeDefs = `
-//     type Query  {
-//       users(query:String):[User!]!
-//       me:User!
-//     }
-
-//     type User {
-//         id: ID!
-//         name:String!
-//         email:String!
-//         age:Int
-//     }
-
-//     type Post {
-//         id:ID!
-//         title:String!
-//         body:String!
-//         published:Boolean!
-//     }
-// `
-// // Resolvers(function)
-// const resolvers = {
-//     Query:{
-//         me(parent,args,ctx,info){
-//             return {
-//                 id:'myId',
-//                 name:'jinsoo',
-//                 age:28,
-//                 email:'roorooro9933@gmail.com'
-//             }
-//         },
-//         users(parent,args,ctx,info){
-//             if(!args.query){
-//                 return users
-//             }
-
-//             return users.filter((user)=>{
-//                 return user.name.toUpperCase().includes(args.query.toUpperCase())
-//             })
-//         }
-//     }
-// }
-
-// const server = new GraphQLServer({
-//     typeDefs,
-//     resolvers
-// })
-
-// server.start(()=>{
-//     console.log('The server is up')
-// })
 
 
